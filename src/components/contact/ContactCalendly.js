5ec347a934dbf4e2a9d401386f2fbc9e46ddc9e7
brettmfox33 <brettmfox33@gmail.com>
2021-05-04 20:48:57 -0600

    Added new images and logos. Updated Calender

--- src/components/contact/ContactCalendly.js
+++ src/components/contact/ContactCalendly.js
@@ -1,13 +1,28 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import { Grid } from '@material-ui/core'
+import { Grid, makeStyles } from '@material-ui/core'
 import {InlineWidget} from 'react-calendly'
 
+const useStyles = makeStyles({
+    iFrame: {
+        width: 420,
+        height: 1880,
+        scrolling: 'yes',
+        border: 0,
+    },
+    iFrameContainer: {
+        height: 2000,
+        marginTop: 50
+    }
+  });
+
+
 export default function ContactCalendly() {
+    const classes = useStyles();
 
  return (
-    <Grid item xs={12} sm={5}>                    
-    <InlineWidget 
+    <Grid item container justify="center" xs={12} sm={5} className={classes.iFrameContainer}>                    
+    {/* <InlineWidget 
         styles={{
             height: '1300px'
         }}
@@ -15,7 +30,14 @@ export default function ContactCalendly() {
             primaryColor: "60258d"
         }}
         url="https://calendly.com/bigbudbookkeeping/15min"
-    />
+    /> */}
+        <iframe 
+            src='https://outlook.office365.com/owa/calendar/BigBudBookkeeping1@bigbudbookkeeping.com/bookings/' 
+            title="booking_iframe"
+            className={classes.iFrame}
+            allowtransparency="true"
+        >
+            </iframe>
 </Grid>
  )
 }
